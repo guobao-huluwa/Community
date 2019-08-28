29d7ad6da22af7c1eb2207c43f75b85a0d1b85e3
li178 <shuguang.li@definesys.com>
2019-08-28 10:06:29 +0800

    first commit

---
+++ src/main/java/com/lsg/community/controller/HellowController.java
@@ -0,0 +1,23 @@
+package com.lsg.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 描述
+ *
+ * @outhor 李曙光
+ * @time 2019-08-27  15:13
+ */
+@Controller
+public class HellowController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name ="name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
