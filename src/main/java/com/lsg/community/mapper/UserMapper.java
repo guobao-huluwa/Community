a3247aab9fd53a5da31edc091fbe27460be26755
li178 <shuguang.li@definesys.com>
2019-09-17 17:04:23 +0800

    add check cookies

--- src/main/java/com/lsg/community/mapper/UserMapper.java
+++ src/main/java/com/lsg/community/mapper/UserMapper.java
@@ -4,6 +4,8 @@ package com.lsg.community.mapper;
 import com.lsg.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * 描述
@@ -15,5 +17,6 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
-
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
